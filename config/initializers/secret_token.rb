54fd9900a5a09a4f46e3e8482d8414799e00b521
bhennes2 <bhennes2@gmail.com>
2013-07-27 12:20:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clueless::Application.config.secret_token = 'fd4aa47bd4613f4cc2529cbfdf882f5da372c77dc5e57f56ad79dfc3ce5b884156b412bd224f88896639f08c28c598ca0cafcc98a2b1360d2d301d68e67b37c1'
